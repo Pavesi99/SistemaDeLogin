adeeacebcaf3dde5772d6973d3d5895d1a41ba6a
Vilmar Pavesi Junior <juniorpavesi999@gmail.com>
2019-07-04 20:32:14 -0300

    Atualição

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <!-- CSS jQuert Validate -->
+    <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
     <title>Sistema de Login</title>
     <style>
         #alerta,#caixaRegistro,#caixaSenha{
@@ -46,7 +47,7 @@
                                  required minlength="6">
                           
                       </div>
-                      <div class="form-group">
+                      <div class="form-group mt-5">
                        <div class="custom-control custom-checkbox">
                            <input type="checkbox" name="lembrar" id="checkLembrar" class="custom-control-input" >
                            <label for="checkLembrar" class="custom-control-label">
@@ -106,11 +107,11 @@
                               
                               <!-- Campo de confirmação de senha -->
                               <div class="form-group">
-                                  <input type="passord" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar a Senha"
+                                  <input type="password" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar a Senha"
                                          required minlength="6">
                               </div>
                               <!-- CheckBox de aceite os termos -->
-                              <div class="form-group">
+                              <div class="form-group mt-5">
                                   <div class="custom-control custom-checkbox">
                                   <input type="checkbox" name="concordar" class="custom-control-input" id="checkConcordar">
                                   <label for="checkConcordar" class="custom-control-label" >
@@ -179,7 +180,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
+    
     <script>
+        
         //$(document).ready(function(){});
        // JQuery
        //Esqueci a senha
@@ -204,7 +208,47 @@
               $("#caixaLogin").show();
                
           });
+          
          });
+             //Validação com jQuery
+                        jQuery.validator.setDefaults({
+             debug: false,
+             success: "valid"
+           });
+         $("#formLogin").validate();
+         $("#formSenha").validate();
+         $("#formRegistro").validate({
+             rules:{
+                 senhaUsuarioConfirmar:{
+                     equalTo: "#senhaUsuario"
+                 }
+                }
+         });
+         
+     
+     /*
+* Tradução padrao do jQuery
+* Locale: PT_BR
+*/
+jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo &eacute; requerido.",
+    remote: "Por favor, corrija este campo.",
+    email: "Por favor, forne&ccedil;a um endere&ccedil;o eletr&ocirc;nico v&aacute;lido.",
+    url: "Por favor, forne&ccedil;a uma URL v&aacute;lida.",
+    date: "Por favor, forne&ccedil;a uma data v&aacute;lida.",
+    dateISO: "Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).",
+    number: "Por favor, forne&ccedil;a um n&uacute;mero v&aacute;lido.",
+    digits: "Por favor, forne&ccedil;a somente d&iacute;gitos.",
+    creditcard: "Por favor, forne&ccedil;a um cart&atilde;o de cr&eacute;dito v&aacute;lido.",
+    equalTo: "Por favor, forne&ccedil;a o mesmo valor novamente.",
+    accept: "Por favor, forne&ccedil;a um valor com uma extens&atilde;o v&aacute;lida.",
+    maxlength: jQuery.validator.format("Por favor, forne&ccedil;a n&atilde;o mais que {0} caracteres."),
+    minlength: jQuery.validator.format("Por favor, forne&ccedil;a ao menos {0} caracteres."),
+    rangelength: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1} caracteres de comprimento."),
+    range: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1}."),
+    max: jQuery.validator.format("Por favor, forne&ccedil;a um valor menor ou igual a {0}."),
+    min: jQuery.validator.format("Por favor, forne&ccedil;a um valor maior ou igual a {0}.")
+});
     </script>
   </body>
 </html>
