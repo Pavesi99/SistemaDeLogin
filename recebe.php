77ebcd92037d6580e0193cb9881b54c61833e989
Vilmar Pavesi Junior <juniorpavesi999@gmail.com>
2019-07-18 21:30:08 -0300

    Alterado gerar nova senha

--- recebe.php
+++ recebe.php
@@ -98,7 +98,12 @@ if(isset($_POST['action'])
                 echo "Algo deu errado. Por favor, tente novamente.";            
         }
     }
-} else {
+}elseif(isset($_POST['action']) 
+        && $_POST['action'] == 'gerar'){
+    $emailGerarSenha = verificar_entrada($_POST['emailGerarSenha']);
+    
+    echo $emailGerarSenha;
+        }else {
     
    header("location:index.php"); //redireciona ao acessar este arquivo diretamente
    //só funciona quando nada esta impresso na tela
